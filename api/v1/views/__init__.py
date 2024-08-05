987cca68d97a948a93da878db6fc3326d3e069fc
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-08-05 22:52:51 +0800

    Create an endpoint that retrieves the number of each objects by type

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,5 +6,5 @@ from flask import Blueprint
 
 app_views = Blueprint('/api/v1', __name__)
 
-
-from api.v1.views.index import *
+if True:
+    from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,11 +3,34 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 
 
-@app_views.route('/api/v1/status', methods=['GET'])
+@app_views.route('/api/v1/status', methods=['GET'], strict_slashes=False)
 def status():
     """
     Returns a JSON response with status OK.
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    Create an endpoint that retrieves the number of each objects by type
+    """
+    return jsonify({
+        'states': storage.count(State),
+        'cities': storage.count(City),
+        'places': storage.count(Place),
+        'amenities': storage.count(Amenity),
+        "reviews": storage.count(Review),
+        "users": storage.count(User)
+        })
