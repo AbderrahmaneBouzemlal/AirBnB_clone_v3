0eecc480efcc3652008197250c1b05519f17bd28
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-08-09 00:20:31 +0800

    updated the package

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,9 +6,8 @@ from flask import Blueprint
 
 app_views = Blueprint('/api/v1', __name__)
 
-if True:
-    from api.v1.views.index import *
-    from api.v1.views.states import *
-    from api.v1.views.cities import *
-    from api.v1.views.amenities import *
-    from api.v1.views.users import *
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
